ece70b33d1582236244b030759600643f8071c69
Krishnan <ks@geedesk.com>
2025-10-06 11:59:05 +0530

    Create ticket page improvements

--- frontend/application/config/prod_config_sample.php
+++ frontend/application/config/prod_config_sample.php
@@ -1,7 +1,7 @@
 <?php
 
-$config['geedesk_secret_key'] = 'eyJ0eXAiOi5bock02uAfe3csiOiJIUzIsuKdcyG5dC';
-$config['geedesk_third_party_secret_key'] = 'eyJ0eXAiOi5bock02uAfeJH675675JHJH8767863csiOiJI';
+$config['geedesk_secret_key'] = '';
+$config['geedesk_third_party_secret_key'] = '';
 
 $config['api_base_url'] = 'http://localhost:8082/v1';
 
@@ -9,7 +9,7 @@ $config['client_id'] = '';
 $config['client_secret'] = '';
 $config['redirect_uri'] = '';
 
-$config['linode_base_url'] = 'https://geedesk-internal-files.us-east-1.linodeobjects.com';
+$config['linode_base_url'] = '';
 
 $config['home_page'] = 'home';
 
